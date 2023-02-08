70ad1ac67dbb8713200546138cb8e6fd5ca3948b
sai230123 <123530088+sai230123@users.noreply.github.com>
2023-02-08 10:40:37 +0530

    4 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { customers } from '../customerData';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
+  customers = customers;
 
   share() {
     window.alert('The product has been shared!');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
