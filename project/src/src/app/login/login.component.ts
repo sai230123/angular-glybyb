6868853a29d7f555738bb98ddd0d6b238a7c1d49
sai230123 <123530088+sai230123@users.noreply.github.com>
2023-02-20 19:34:36 +0530

    3 files modified, 19 deleted

--- project/src/src/app/login/login.component.ts
+++ project/src/src/app/login/login.component.ts
@@ -8,44 +8,52 @@ import { ToastService } from '../shared/toast.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   showSpinner: boolean = false;
   loginForm: FormGroup;
 
-  constructor(private service: CapstoneService, private toastService: ToastService, private router: Router) {
+  constructor(
+    private service: CapstoneService,
+    private toastService: ToastService,
+    private router: Router
+  ) {
     this.loginForm = new FormGroup({
       loginId: new FormControl('', Validators.required),
-      psw: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}$')])
+      psw: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-zd$@$!%*?&].{7,}$'
+        ),
+      ]),
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   doLogin() {
     const loginRequest: LoginRequest = {
       loginId: this.loginForm.get('loginId')?.value,
-      pwd: this.loginForm.get('psw')?.value
-    }
+      pwd: this.loginForm.get('psw')?.value,
+    };
 
     this.showSpinner = true;
-    this.service.login(loginRequest).subscribe((res: CapstoneResponse) => {
-      this.showSpinner = false;
-      console.log(res);
-      if(res.success) {
-        this.toastService.show('I am a success toast');
-        this.router.navigate(['/home' , {}]);
-      }else {
-        this.toastService.show('I am a error toast', {status : 'error'});
+    this.service.login(loginRequest).subscribe(
+      (res: CapstoneResponse) => {
+        this.showSpinner = false;
+        console.log(res);
+        if (res.success) {
+          this.toastService.show('I am a success toast');
+          this.router.navigate(['/home', {}]);
+        } else {
+          this.toastService.show('I am a error toast', { status: 'error' });
+        }
+      },
+      (error) => {
+        this.showSpinner = false;
+        this.toastService.show('I am a error toast', { status: 'error' });
       }
-      
-    }, error => {
-      this.showSpinner = false;
-      this.toastService.show('I am a error toast', {status : 'error'}
-    })
+    );
   }
-
 }
